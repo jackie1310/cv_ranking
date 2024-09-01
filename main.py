db6c6211f0cdc84be3e488f198249a9520d96c7e
jackie1310 <an.nguyenviet.13.10@gmail.com>
2024-09-01 23:49:02 +0700

    add origins

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI(title=settings.APP_NAME)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://cv-ranking-front-end.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
